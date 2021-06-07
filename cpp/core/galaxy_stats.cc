86add99c5b501a11f3887e3dcccdd4d598d37f65
] <kfrancischen@gmail.com>
2021-06-07 09:25:24 -0700

    add back the latency distribution

--- cpp/core/galaxy_stats.cc
+++ cpp/core/galaxy_stats.cc
@@ -11,7 +11,9 @@ namespace galaxy {
                 .set_name("galaxy_server/latency")
                 .set_description("The various methods' latencies in milliseconds")
                 .set_measure(internal::kLatencyMeasureName)
-                .set_aggregation(opencensus::stats::Aggregation::LastValue())
+                .set_aggregation(opencensus::stats::Aggregation::Distribution(
+                    opencensus::stats::BucketBoundaries::Explicit(
+                        {0, 25, 50, 75, 100, 200, 400, 800, 1000, 4000})))
                 .add_column(internal::MethodKey());
             opencensus::stats::View server_view(server_latency_view);
             CHECK(server_view.IsValid()) << "Failed to create server latency view.";
