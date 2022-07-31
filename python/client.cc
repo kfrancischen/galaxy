ef9c27c7e42e39733d49003f0c2f7ed449a6634b
] <kfrancischen@gmail.com>
2022-07-31 13:36:49 -0700

    add include_hidden to python wrapper

--- python/client.cc
+++ python/client.cc
@@ -14,12 +14,12 @@ PYBIND11_MODULE(_gclient, m)
     m.def("create_dir_if_not_exist", &galaxy::client::CreateDirIfNotExist, "Wrapper for CreateDirIfNotExist",
         py::arg("path"), py::arg("mode") = 0777);
     m.def("dir_or_die", &galaxy::client::DirOrDie, "Wrapper for DirOrDie", py::arg("path"));
-    m.def("rm_dir", &galaxy::client::RmDir, "Wrapper for RmDir", py::arg("path"));
-    m.def("rm_dir_recursive", &galaxy::client::RmDirRecursive, "Wrapper for RmDirRecursive", py::arg("path"));
+    m.def("rm_dir", &galaxy::client::RmDir, "Wrapper for RmDir", py::arg("path"), py::arg("include_hidden")=false);
+    m.def("rm_dir_recursive", &galaxy::client::RmDirRecursive, "Wrapper for RmDirRecursive", py::arg("path"), py::arg("include_hidden")=false);
     m.def("list_dirs_in_dir", &galaxy::client::ListDirsInDir, "Wrapper for ListDirsInDir", py::arg("path"));
-    m.def("list_files_in_dir", &galaxy::client::ListFilesInDir, "Wrapper for ListFilesInDir", py::arg("path"));
+    m.def("list_files_in_dir", &galaxy::client::ListFilesInDir, "Wrapper for ListFilesInDir", py::arg("path"), py::arg("include_hidden")=false);
     m.def("list_dirs_in_dir_recursive", &galaxy::client::ListDirsInDirRecursive, "Wrapper for ListDirsInDirRecursive", py::arg("path"));
-    m.def("list_files_in_dir_recursive", &galaxy::client::ListFilesInDirRecursive, "Wrapper for ListFilesInDirRecursive", py::arg("path"));
+    m.def("list_files_in_dir_recursive", &galaxy::client::ListFilesInDirRecursive, "Wrapper for ListFilesInDirRecursive", py::arg("path"), py::arg("include_hidden")=false);
     m.def("create_file_if_not_exist", &galaxy::client::CreateFileIfNotExist, "Wrapper for CreateFileIfNotExist",
         py::arg("path"), py::arg("mode") = 0777);
     m.def("file_or_die", &galaxy::client::FileOrDie, "Wrapper for FileOrDie", py::arg("path"));
@@ -37,8 +37,8 @@ PYBIND11_MODULE(_gclient, m)
         }
         return result;
     }, "Wrapper for ReadMultiple", py::arg("paths"));
-    m.def("write", &galaxy::client::Write, "Wrapper for Write", py::arg("path"), py::arg("data"), py::arg("mode") = "w");
-    m.def("write_multiple", &galaxy::client::WriteMultiple, "Wrapper for WriteMultiple", py::arg("path_data_map"), py::arg("mode") = "w");
+    m.def("write", &galaxy::client::Write, "Wrapper for Write", py::arg("path"), py::arg("data"), py::arg("mode")="w");
+    m.def("write_multiple", &galaxy::client::WriteMultiple, "Wrapper for WriteMultiple", py::arg("path_data_map"), py::arg("mode")="w");
     m.def("get_attr", &galaxy::client::GetAttr, "Wrapper for GetAttr", py::arg("path"));
     m.def("list_cells", &galaxy::client::ListCells, "Wrapper for ListCells");
     m.def("check_health", &galaxy::client::CheckHealth, "Wrapper for CheckHealth", py::arg("cell"));
